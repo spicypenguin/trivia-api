7f41c56f7db0aaf72dad1801dd6e8d8dab641e69
JSinks <jeff@fatpengu.in>
2020-05-19 21:21:43 -0700

    correcting test to work on windows

--- backend/bin/test.sh
+++ backend/bin/test.sh
@@ -1,4 +1,4 @@
-dropdb trivia_test
-createdb trivia_test
-psql trivia_test < trivia.psql
+sudo -u postgres dropdb trivia_test
+sudo -u postgres createdb trivia_test
+sudo -u postgres psql trivia_test < trivia.psql
 python test_flaskr.py
